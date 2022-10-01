ee618ad693eea170f7ea9dda9952051ba03f30b7
corkr900 <109830893+corkr900@users.noreply.github.com>
2022-10-01 11:39:21 -0500

    update version

--- Code/Head2HeadModule.cs
+++ Code/Head2HeadModule.cs
@@ -37,7 +37,7 @@ namespace Celeste.Mod.Head2Head {
 		internal int MatchTimeoutMinutes = 15;
 
 		// Constants that might change in the future
-		public static readonly string ProtocolVersion = "1_0_6";
+		public static readonly string ProtocolVersion = "1_0_7";
 
 		// Other static stuff
 		public static Head2HeadModule Instance { get; private set; }
