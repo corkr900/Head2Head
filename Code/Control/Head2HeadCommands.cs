dc675e29744fbe260fa8c17c533ac70ea58d43e2
corkr900 <109830893+corkr900@users.noreply.github.com>
2023-10-19 22:11:27 -0500

    start building randomizer integration

--- Code/Control/Head2HeadCommands.cs
+++ Code/Control/Head2HeadCommands.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Celeste.Mod.Head2Head.Control {
@@ -122,6 +123,108 @@ namespace Celeste.Mod.Head2Head.Control {
 		//	Head2HeadModule.Instance.StageMatch();
 		//	Engine.Commands.Log("Done");
 		//}
+
+		[Command("h2h_rando", "buh")]
+		public static void Rando(string arg) {
+			Thread BuilderThread = null;
+			object Settings;
+			DynamicData dd_Settings = null;
+			Func<object, AreaKey> RandoLogic_GenerateMap = null;
+			object RandoModuleInstance = null;
+			DynamicData dd_RandoModuleInstance = null;
+			Action<AreaKey?> RandoModule_StartMe_Set = null;
+			Func<AreaKey?> RandoModule_StartMe_Get = null;
+			Action<object, object> RandoModule_SavedData_SavedSettings_Set = null;
+
+			try {
+				Type t_RandoSettings = Type.GetType("Celeste.Mod.Randomizer.RandoSettings,Randomizer");
+				Settings = Activator.CreateInstance(t_RandoSettings);
+				dd_Settings = DynamicData.For(Settings);
+
+				Type t_RandoLogic = Type.GetType("Celeste.Mod.Randomizer.RandoLogic,Randomizer");
+
+				MethodInfo m_RandoLogic_GenerateMap = t_RandoLogic.GetMethod("GenerateMap", BindingFlags.Static | BindingFlags.Public);
+				RandoLogic_GenerateMap = (object settings)
+					=> (AreaKey)m_RandoLogic_GenerateMap.Invoke(null, new object[] { settings });
+				
+				Type t_RandoModule = Type.GetType("Celeste.Mod.Randomizer.RandoModule,Randomizer");
+				FieldInfo f_RandoModule_Instance = t_RandoModule.GetField("Instance", BindingFlags.Static | BindingFlags.Public);
+				RandoModuleInstance = f_RandoModule_Instance.GetValue(null);
+				dd_RandoModuleInstance = DynamicData.For(RandoModuleInstance);
+				PropertyInfo p_RandoModule_SavedData = t_RandoModule.GetProperty("SavedData", BindingFlags.Instance | BindingFlags.Public);
+				Type t_RandoModuleSettings = p_RandoModule_SavedData.PropertyType;
+				PropertyInfo p_RandoModuleSettings_SavedSettings = t_RandoModuleSettings.GetProperty("SavedSettings", BindingFlags.Instance | BindingFlags.Public);
+				RandoModule_SavedData_SavedSettings_Set = (object randoModuleInstance, object settings) => {
+					object SavedData = p_RandoModule_SavedData.GetValue(randoModuleInstance);
+					p_RandoModuleSettings_SavedSettings.SetValue(SavedData, settings);
+				};
+				FieldInfo f_RandoModule_StartMe = t_RandoModule.GetField("StartMe", BindingFlags.Static | BindingFlags.Public);
+				RandoModule_StartMe_Set = (AreaKey? key) => {
+					f_RandoModule_StartMe.SetValue(null, key);
+				};
+				RandoModule_StartMe_Get = () => {
+					return (AreaKey?)f_RandoModule_StartMe.GetValue(null);
+				};
+			}
+			catch (Exception e) {
+				return;
+			}
+
+			dd_Settings.Invoke("EnableMap", new AreaKey(0, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(1, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(2, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(3, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(4, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(5, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(6, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(7, AreaMode.Normal));
+			//dd_Settings.Invoke("EnableMap", new AreaKey(8, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(9, AreaMode.Normal));
+			dd_Settings.Invoke("EnableMap", new AreaKey(10, AreaMode.Normal));
+
+			BuilderThread = new Thread(() => {
+				//Settings.Enforce();
+				dd_Settings.Invoke("Enforce");
+
+				AreaKey newArea;
+				try {
+					newArea = RandoLogic_GenerateMap(Settings);
+				}
+				catch (ThreadAbortException) {
+					return;
+				}
+				//catch (GenerationError e) {
+				//	//errortext.Title = e.Message;
+				//	//errortext.FadeVisible = true;
+				//	//reenableMenu();
+				//	return;
+				//}
+				catch (Exception e) {
+					//errortext.Title = "Encountered an error - Check log.txt for details";
+					//Logger.LogDetailed(e, "randomizer");
+					//errortext.FadeVisible = true;
+					//reenableMenu();
+					Engine.Commands.Log(e.InnerException?.Message);
+					return;
+				}
+
+				// save settings
+				//RandoModule.Instance.SavedData.SavedSettings = Settings.Copy();
+				RandoModule_SavedData_SavedSettings_Set(RandoModuleInstance, dd_Settings.Invoke("Copy"));
+				//RandoModule.Instance.SaveSettings();
+				dd_RandoModuleInstance.Invoke("SaveSettings");
+
+				//RandoModule.StartMe = newArea;
+				RandoModule_StartMe_Set(newArea);
+
+				//while (RandoModule.StartMe != null) {
+				while (RandoModule_StartMe_Get() != null) {
+					Thread.Sleep(10);
+				}
+				BuilderThread = null;
+			});
+			BuilderThread.Start();
+		}
 
 		[Command("h2h_hidehelpdesk", "Toggles the helpdesk pause menu option. Pass 1 as the first argument to hide, 0 to show, or nothing to toggle.")]
 		public static void SetHelpdeskPauseVisible(string arg) {
