383c8924d0fde4686750543cea431f3e6b5863d3
corkr900 <109830893+corkr900@users.noreply.github.com>
2024-07-02 07:19:57 -0500

    fix match logging setting affecting control panel

--- Code/Head2HeadModuleSettings.cs
+++ Code/Head2HeadModuleSettings.cs
@@ -135,8 +135,6 @@ namespace Celeste.Mod.Head2Head {
 					EnabledState cur = EnableMatchLogs ? EnabledState.Enabled : EnabledState.Disabled;
 					if (cur == val) return;
 					EnableMatchLogs = val == EnabledState.Enabled;
-					if (EnableMatchLogs) ControlPanelCore.TryInitServer();
-					else ControlPanelCore.EndServer();
 				});
 			AddSlider(menu, "Head2Head_Setting_HudScale", HudScale,
 				new float[] { 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 2.0f },
